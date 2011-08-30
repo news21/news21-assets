d131a18e1b31bbc2ce23eeae8f9ee0ef66d9f783
Mark Ng <mng@jrn206b.dhcp.asu.edu>
2011-08-30 16:39:43 -0700

    reduce standard size.

--- 2011/importsmap.js
+++ 2011/importsmap.js
@@ -3,7 +3,7 @@ wax.tilejson(url, function(tilejson) {
   var mm = com.modestmaps;
   var m = new mm.Map('modestmaps-setup',
     new wax.mm.connector(tilejson),
-    new mm.Point(1024,768));
+    new mm.Point(850,680));
   m.setCenterZoom(new mm.Location(
     tilejson.center[1],  // lon
     tilejson.center[0]), // lat
