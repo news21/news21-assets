c8675f4b5c46890584f09379882e142b996b7099
Mark Ng <mng@jrn206b.dhcp.asu.edu>
2011-08-30 16:31:22 -0700

    Add importsmapjs

---
+++ 2011/importsmap.js
@@ -0,0 +1,13 @@
+var url = 'http://maps.news21.com/api/Tileset/imports'
+wax.tilejson(url, function(tilejson) {
+  var mm = com.modestmaps;
+  var m = new mm.Map('modestmaps-setup',
+    new wax.mm.connector(tilejson),
+    new mm.Point(1024,768));
+  m.setCenterZoom(new mm.Location(
+    tilejson.center[1],  // lon
+    tilejson.center[0]), // lat
+    tilejson.center[2]); // zoom
+  wax.mm.interaction(m, tilejson);
+  wax.mm.fullscreen(m, tilejson).appendTo(m.parent);
+});
